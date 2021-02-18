77f5acbbb8fc2b95d9c4847a2c1619f1577d1aa0
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-18 15:55:20 +0000

    changed stuff

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function epicFunction( ){
   let randomnum = Math.floor(Math.random() * 6) + 1
   if (userguess == randomnum) {
     alert('Haha! I guessed your number. It was ' + userguess + '!')
+  } else if (userguess > 6) {
+    alert('Come on man, I said 1 to 6!')
   } else {
     alert('Dang it, I thought it was ' + randomnum + '.')
   }
